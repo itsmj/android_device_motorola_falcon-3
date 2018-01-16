db8e9d8bf9e49e8831f3b2897aed427232fc3611
Gaurav <allstargaurav@gmail.com>
2018-01-16 22:01:25 +0530

    Define Bootanimation Resolution

--- aosp_falcon.mk
+++ aosp_falcon.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 # Inherit some common aosp stuff.
 $(call inherit-product, vendor/aosp/common.mk)
 
+#Bootanimation res
+TARGET_BOOT_ANIMATION_RES := 720
+
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := aosp_falcon
 
